f4c71eaaad6f560319f92041fb476822b8d6c48e
Romy Valdez <romyvs01@gmail.com>
2026-02-17 21:30:35 -0400

    Habilitado script en layout que evitaba renderizado en layout principal

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" type="text/css" href="/fonts/themify/themify-icons.css" />
         <link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css" />
         <link rel="stylesheet" type="text/css" href="/vendor/css-hamburgers/hamburgers.min.css" />
-        {/* <link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css" /> */}
+        <link rel="stylesheet" type="text/css" href="/vendor/animsition/css/animsition.min.css" />
         <link rel="stylesheet" type="text/css" href="/vendor/select2/select2.min.css" />
         <link rel="stylesheet" type="text/css" href="/vendor/daterangepicker/daterangepicker.css" />
         <link rel="stylesheet" type="text/css" href="/vendor/slick/slick.css" />
