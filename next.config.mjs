e1fdff022a265c01855321a1778114418c2e7877
derel_portorreal <derel_portorreal@jce.do>
2026-04-23 11:19:10 -0400

    fix mejoras de bot

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  experimental: {
-    turbopack: {
-      root: '.',
-    },
+  turbopack: {
+    root: '.',
   },
 };
 
